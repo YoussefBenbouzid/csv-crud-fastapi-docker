0a525ec49ab83a5a75fcc536ff9e503728266e75
Youssef Benbouzid <youssef.benbouzid@outlook.com>
2024-08-07 18:56:27 +0200

    aggiorno

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Il progetto permette di gestire un file CSV attraverso le operazioni CRUD (Creat
 ## Configurazione dell'environment
 Viene utilizzato Anaconda, piattaforma open source che permette di creare environment indipendenti.
 
-Attraverso Anaconda Prompt si è digitato il seguente comando, che permette di creare un environment chiamato "project" e che fa riferimento alla versione 3.12 di Python e che permette di installare nell'environment vari pacchetti, in particolare Python e PIP:
+Attraverso Anaconda Prompt si è digitato il seguente comando, che permette di creare un environment chiamato "project" e che fa riferimento alla versione 3.12 di Python e che permette di installare nell'environment vari pacchetti, in particolare Pip:
 
 ```bash
 conda create --name project python=3.12
@@ -18,15 +18,59 @@ Il seguente comando attiva l'environment:
 conda activate project
 ```
 
-Le dipendenze vengono impostate con le relative versioni nel file requirements.txt (salvato nella directory del progetto) e gestito attraverso PIP:
+Le dipendenze vengono impostate con le relative versioni nel file requirements.txt (salvato nella directory del progetto) e gestito attraverso Pip:
 
 ```bash
 pip install -r requirements.txt
 ```
 
-Tra le varie dipendenze si cita in particolare Uvicorn, server di tipo ASGI (Asynchronous Server Gateway Interface), utilizzato per questo progetto.
-
 ## Implementazione
-Viene utilizzato FastAPI, framework web di Python che permette di creare API.
+Viene utilizzato FastAPI, framework web di Python che permette di creare API e gestire operazioni CRUD.
 
-I dati del file CSV includono id, nome, cognome e codice fiscale e sono stati impostati con la libreria Pydantic.
+I dati del file CSV includono id, nome, cognome e codice fiscale e sono stati validati attraverso la classe BaseModel della libreria Pydantic.
+
+Per intervenire sul file CSV viene importata la libreria csv.
+
+Tra le varie dipendenze si cita poi in particolare Uvicorn, server di tipo ASGI (Asynchronous Server Gateway Interface), utilizzato per eseguire l'applicazione.
+
+### Funzioni
+
+#### aggiungiRecord(item: Item)
+
+Apre il file CSV in modalità append e vi aggiunge un nuovo record contenente i dati dell'item in ingresso.
+
+#### recuperaRecordTutti()
+
+Apre il file CSV in modalità read e ne copia tutti i record in una lista, che viene restituita.
+
+#### recuperaRecordUno(id: int)
+Apre il file CSV in modalità read e ne legge i record; se trova l'id in ingresso restituisce il relativo item, altrimenti restituisce un messaggio di errore.
+
+#### modificaRecord(id: int, nuovoItem: Item)
+Apre il file CSV in modalità read e controlla gli id di tutti i record; se l'id in ingresso viene trovato la funzione sostituisce i campi del relativo item con i valori del nuovo item in ingresso; tutti i record vengono poi copiati in una lista, che viene usata per sovrascrivere il file CSV. La funzione restituisce il valore di un flag che segnala se l'id è stato trovato.
+
+#### eliminaRecord(id: int)
+Apre il file CSV in modalità read e ne copia tutti i record in una lista, ad eccezione del record il cui id è quello in ingresso. La lista viene usata per sovrascrivere il file CSV. La funzione restituisce il valore di un flag che segnala se l'id è stato trovato.
+
+#### numeroRecord()
+Apre il file CSV in modalità read e ne conta il numero dei record attraverso una variabile contatore, che viene restituita.
+
+### Endpoint
+
+#### Endpoint per la creazione di un nuovo record:
+Metodo: POST, Path: /items/
+
+#### Endpoint per ottenere tutti i record:
+Metodo: GET, Path: /items/
+
+#### EEndpoint per ottenere un singolo record basato sull'ID:
+Metodo: GET, Path: /items/{id}
+
+#### EEndpoint per aggiornare un record esistente:
+Metodo: PUT, Path: /items/{id}
+
+#### EEndpoint per eliminare un record esistente:
+Metodo: DELETE, Path: /items/{id}
+
+#### EEndpoint per ottenere il numero di righe nel CSV:
+Metodo: GET, Path: /items/count
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-#Importo le librerie che mi servono
+#Importo librerie
 from fastapi import FastAPI
 from pydantic import BaseModel
-import csv
-import os
-import uvicorn
+from fastapi.responses import JSONResponse
 from typing import List
+import csv
+import uvicorn
 
 app = FastAPI() #Istanzio applicazione FastAPI
 file_csv = 'file.csv' #Creo variabile che contiene il nome del file CSV
@@ -16,114 +16,172 @@ class Item(BaseModel):
     cognome: str
     codice_fiscale: str
 
-#Funzione per aggiungere un nuovo record nel CSV
+#Funzione per aggiungere un nuovo record nel file CSV
 def aggiungiRecord(item: Item):
-    with open(file_csv, mode='a') as file: #Apro il file CSV in modalità "a" ("append")
-        writer = csv.writer(file) #Definisco oggetto per scrivere i dati nel file CSV
+    with open(file_csv, mode='a', newline="") as file: #Apro il file CSV in modalità "a" ("append")
+        writer = csv.writer(file) #Creo oggetto writer per scrivere sul file CSV
         writer.writerow([item.id, item.nome, item.cognome, item.codice_fiscale]) #Aggiungo una singola riga al file CSV usando l'oggetto writer e il metodo writerow
     
-#Funzione che restituisce i record nel file CSV come lista di oggetti Item
-def recuperaRecordTutti(): #Segnalo che restituisco una lista
-    listaRecord = [] #Creo una lista vuota
-    if os.path.isfile(file_csv): #Controllo se il file esiste
-        with open(file_csv, mode='r') as file: #Apro il file CSV in modalità "r" ("read")
-            reader = csv.DictReader(file) #Creo un lettore che restituisce i record come dizionari, dei quali ogni colonna è una chiave
-            for row in reader: #Per ogni ogni riga del file CSV aggiungo alla lista un oggetto Item
-                listaRecord.append(Item(
+#Funzione che restituisce i record del file CSV come lista di oggetti Item
+def recuperaRecordTutti():
+    lista = [] #Creo lista vuota
+    with open(file_csv, mode='r', newline="") as file: #Apro il file CSV in modalità "r" ("read")
+        reader = csv.DictReader(file) #Creo oggetto reader che restituisce i record come dizionari, dei quali ogni campo è una chiave
+        #Per ogni riga del file CSV aggiungo alla lista un oggetto Item
+        for row in reader:
+            lista.append(Item(
+                id=int(row['id']),
+                nome=row['nome'],
+                cognome=row['cognome'],
+                codice_fiscale=row['codice_fiscale']
+            ))
+    return lista #Restituisco la lista
+
+#Funzione che restituisce un record del file CSV  
+def recuperaRecordUno(id: int): #Segnalo che restituisco un item
+    trovato = False #Flag per verificare se l'id ricercato è stato trovato
+    with open(file_csv, mode='r', newline="") as file: #Apro il file CSV in modalità "r" ("read")
+        reader = csv.DictReader(file) #Creo oggetto reader che restituisce i record come dizionari, dei quali ogni campo è una chiave
+        #Per ogni riga del file CSV verifico se l'id è simile a quello ricercato, se la condizione è vera restituisco l'item ricercato
+        for row in reader:
+            if int(row['id']) == id:
+                trovato = True
+                return Item(
                     id=int(row['id']),
                     nome=row['nome'],
                     cognome=row['cognome'],
                     codice_fiscale=row['codice_fiscale']
-                ))
-    return listaRecord #Restituisco la lista
-
-#Funzione che restituisce un record del file CSV  
-def recuperaRecordUno(id: int): #Segnalo che restituisco un item
-    if os.path.isfile(file_csv): #Controllo se il file CSV esiste
-        with open(file_csv, mode='r') as file: #Apro il file CSV in modalità "r" ("read")
-            reader = csv.DictReader(file) #Creo un lettore che restituisce i record come dizionari, dei quali ogni colonna è una chiave
-            for row in reader:
-                if int(row['id']) == id: #Per ogni ogni riga del file CSV verifico se l'id è simile a quello ricercato
-                    return Item( #Se la condizione è vera ritorno l'item ricercato
-                        id=int(row['id']),
-                        nome=row['nome'],
-                        cognome=row['cognome'],
-                        codice_fiscale=row['codice_fiscale']
-                    )
+                )
+            if trovato == False:
+                response = {
+                    "message": "Item not found"
+                }
+                return JSONResponse(content=response)
 
 #Funzione che modifica un record del file CSV
 def modificaRecord(id: int, nuovoItem: Item):
-    listaRecord = []
-    if os.path.isfile(file_csv): #Controllo se il file CSV esiste
-        with open(file_csv, mode='r') as file: #Apro il file CSV in modalità "r" ("read")
-            reader = csv.DictReader(file) #Creo un lettore che restituisce i record come dizionari, dei quali ogni colonna è una chiave
-            for row in reader:
-                if int(row['id']) == id: #Per ogni ogni riga del file CSV verifico se l'id è simile a quello ricercato
-                    id=int(row['id']),
-                    row['nome'] = nuovoItem.nome
-                    row['cognome'] = nuovoItem.cognome
-                    row['codice_fiscale'] = nuovoItem.codice_fiscale
-                listaRecord.append(row)
-        with open(file_csv, mode='w') as file:
-            fieldnames = ['id', 'nome', 'cognome', 'codice_fiscale']
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerows(listaRecord)
+    trovato = False #Flag per verificare se l'id ricercato è stato trovato
+    lista = [] #Creo lista vuota
+    with open(file_csv, mode='r', newline="") as file: #Apro il file CSV in modalità "r" ("read")
+        reader = csv.DictReader(file) #Creo un oggetto reader che restituisce i record come dizionari, dei quali ogni campo è una chiave
+        #Per ogni riga del file CSV verifico se l'id è simile a quello ricercato e nel caso lo sostituisco; aggiungo tutte le righe alla lista
+        for row in reader:
+            if int(row['id']) == id:
+                trovato = True
+                row['nome'] = nuovoItem.nome
+                row['cognome'] = nuovoItem.cognome
+                row['codice_fiscale'] = nuovoItem.codice_fiscale
+            lista.append(row)
+    #Sovrascrivo il file CSV
+    with open(file_csv, mode='w', newline="") as file: #Apro il file CSV in modalità "w" ("write")
+        fieldnames = ['id', 'nome', 'cognome', 'codice_fiscale'] #Specifico i campi
+        writer = csv.DictWriter(file, fieldnames=fieldnames) #Creo un oggetto writer che scrive nel file CSV i record come dizionari
+        writer.writeheader() #Scrivo l'intestazione
+        writer.writerows(lista) #Scrivo nel file CSV le righe contenute nella lista
+    return trovato
 
 #Funzione che elimina un record del file CSV
 def eliminaRecord(id: int):
-    listaRecord = []
-    if os.path.isfile(file_csv): #Controllo se il file CSV esiste
-        with open(file_csv, mode='r') as file: #Apro il file CSV in modalità "r" ("read")
-            reader = csv.DictReader(file) #Creo un lettore che restituisce i record come dizionari, dei quali ogni colonna è una chiave
-            for row in reader:
-                if int(row['id'] != id):
-                    listaRecord.append(row)
-        with open(file_csv, mode='w') as file:
-            fieldnames = ['id', 'nome', 'cognome', 'codice_fiscale']
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerows(listaRecord)
+    trovato = False #Flag per verificare se l'id ricercato è stato trovato
+    lista = [] #Creo lista vuota
+    with open(file_csv, mode='r', newline="") as file: #Apro il file CSV in modalità "r" ("read")
+        reader = csv.DictReader(file) #Creo oggetto reader che restituisce i record come dizionari, dei quali ogni campo è una chiave
+        #Per ogni riga del file CSV verifico se l'id è diverso a quello ricercato e nel caso aggiungo la riga alla lista
+        for row in reader:
+            if int(row['id']) == id:
+                trovato = True
+            if int(row['id']) != id:
+                lista.append(row)
+    #Sovrascrivo il file CSV
+    with open(file_csv, mode='w', newline="") as file: #Apro il file CSV in modalità "w" ("write")
+        fieldnames = ['id', 'nome', 'cognome', 'codice_fiscale'] #Specifico i campi
+        writer = csv.DictWriter(file, fieldnames=fieldnames) #Creo un oggetto writer che scrive nel file CSV i record come dizionari
+        writer.writeheader() #Scrivo l'intestazione
+        writer.writerows(lista) #Scrivo nel file CSV le righe contenute nella lista
+    return trovato
 
 #Funzione che restituisce il numero dei record del file CSV
 def numeroRecord():
-    count = 0 #Dichiaro variabile contatore con valore iniziale 0
-    if os.path.isfile(file_csv): #Controllo se il file esiste
-        with open(file_csv, mode='r') as file:
-            reader = csv.reader(file)
-            for row in reader: #Per ogni riga nel file CSV incremento il valore del contatore
-                count += 1
+    count = 0 #Dichiaro variabile contatore con valore iniziale uguale a zero
+    with open(file_csv, mode='r', newline="") as file: #Apro il file CSV in modalità "r" ("read")
+        reader = csv.reader(file) #Creo oggetto reader per leggere il file CSV
+        #Per ogni riga nel file CSV incremento il valore del contatore
+        for row in reader:
+            count += 1
     return count #Restuisco la variabile contatore
 
-#Endpoint per la creazione di un nuovo record: POST, Path: /items/
+#Endpoint per la creazione di un nuovo record: Metodo: POST, Path: /items/
 @app.post("/items/")
 def aggiungiItem(item: Item):
     aggiungiRecord(item) #Richiamo funzione
+    response = {
+        "id": item.id,
+        "nome": item.nome,
+        "cognome": item.cognome,
+        "codice_fiscale": item.codice_fiscale
+    }
+    return JSONResponse(content=response)
 
-#Endpoint per ottenere tutti i record: GET, Path: /items/
+#Endpoint per ottenere tutti i record: Metodo: GET, Path: /items/
 @app.get("/items/")
 def recuperaItemTutti():
-    listaRecord = recuperaRecordTutti() #Richiamo funzione
+    lista = recuperaRecordTutti() #Richiamo funzione
+    response = [item.dict() for item in lista]  #Converto gli oggetti Item in dizionari
+    return JSONResponse(content=response)
 
-#Endpoint per ottenere un singolo record basato sull'ID: GET, Path: /items/{id}
+#Endpoint per ottenere un singolo record basato sull'ID: Metodo: GET, Path: /items/{id}
 @app.get("/items/{id}")
-def recuperaItemUno(idRecord: int):
-    record = recuperaRecordUno(idRecord) #Richiamo funzione
+def recuperaItemUno(id: int):
+    item = recuperaRecordUno(id) #Richiamo funzione
+    response = {
+        "id": item.id,
+        "nome": item.nome,
+        "cognome": item.cognome,
+        "codice_fiscale": item.codice_fiscale
+    }
+    return JSONResponse(content=response)
 
-#Endpoint per aggiornare un record esistente: PUT, Path: /items/{id}
+#Endpoint per aggiornare un record esistente: Metodo: PUT, Path: /items/{id}
 @app.put("/items/{id}")
 def modificaItem(id: int, nuovoItem: Item):
-    record = modificaRecord(id, nuovoItem) #Richiamo funzione
+    trovato = modificaRecord(id, nuovoItem) #Richiamo funzione
+    if trovato == False:
+        response = {
+            "message": "Item not found"
+        }
+        return JSONResponse(content=response)
+    if trovato == True:
+        response = {
+            "id": nuovoItem.id,
+            "nome": nuovoItem.nome,
+            "cognome": nuovoItem.cognome,
+            "codice_fiscale": nuovoItem.codice_fiscale
+        }
+    return JSONResponse(content=response)
 
-#Endpoint per eliminare un record esistente: DELETE, Path: /items/{id}
+#Endpoint per eliminare un record esistente: Metodo: DELETE, Path: /items/{id}
 @app.delete("/items/{id}")
 def eliminaItem(id: int):
-    eliminaRecord(id)
+    trovato = eliminaRecord(id)
+    if trovato == False:
+        response = {
+            "message": "Item not found"
+        }
+        return JSONResponse(content=response)
+    if trovato == True:
+        response = {
+            "message": "Item deleted successfully"
+        }
+    return JSONResponse(content=response)
 
-#Endpoint per ottenere il numero di righe nel CSV: GET, Path: /items/count
+#Endpoint per ottenere il numero di righe nel CSV: Metodo: GET, Path: /items/count
 @app.get("/items/count")
 def numeroItem():
     count = numeroRecord() #Richiamo funzione
+    response = {
+        "count": count
+    }
+    return JSONResponse(content=response)
 
 #Eseguo l'applicazionne con server uvicorn su porta 8000 e su tutte le interfacce di rete
 uvicorn.run(app, host="0.0.0.0", port=8000)
